0671e4a2facac5d8d9d44dfd294bfa9eb6a76d9c
Brandon Little <blittle@inco.cc>
2024-06-23 11:45:49 -0400

    Create piano entity

---
+++ lua/entities/base_piano/init.lua
@@ -0,0 +1,5 @@
+-- See LICENSE file for copyright and license details.
+
+AddCSLuaFile("cl_init.lua")
+
+include("shared.lua")
---
+++ lua/entities/base_piano/shared.lua
@@ -0,0 +1,6 @@
+-- See LICENSE file for copyright and license details.
+
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_anim"
